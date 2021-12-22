de8fcf8c12024904826de46305b5be0a1bde7ded
helidenial <helidenial8@gmail.com>
2021-12-23 03:38:07 +0530

    Now port number will also be displayed

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000 
 
-app.get('/',(req,res)=>{ console.log(req.socket.remoteAddress+"Logged in");
-res.send('Hello World! at: ' + req.socket.remoteAddress);
+app.get('/',(req,res)=>{ console.log("Remote Address: "+req.socket.remoteAddress+"\nLogged in\n Listening at port "+port);
+res.send("Remote Address: "+req.socket.remoteAddress+"\nLogged in\n Listening at port "+port);
 })
 
 app.listen(port, ()=> { 
